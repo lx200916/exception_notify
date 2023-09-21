007b756e96ae4db84e40f4860f3be1e7a690731e
Lee <44310445+lx200916@users.noreply.github.com>
2023-09-21 21:35:47 +0800

    refactor:Code ReArrangements.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import ExceptionNotify
-ExceptionNotify.install(config_path="./test.toml")
+ExceptionNotify.install()
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    ExceptionNotify.update_info({"Arg1": "V2"})
     print(462//0)
+    ExceptionNotify.update_info({"Arg1": "V3"})
+
 
 
 # Press the green button in the gutter to run the script.
