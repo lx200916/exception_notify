1e2abde527760ff241311e3f3ea338302ea9f001
Lee <44310445+lx200916@users.noreply.github.com>
2023-09-23 12:13:11 +0800

    Chaos: Update Version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="exception_notify",
-    version="0.3",
+    version="0.4",
     author="SaltedFish",
     packages=find_packages(),
     install_requires=[
